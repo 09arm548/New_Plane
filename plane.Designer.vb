eb4e354dad72f209f2ebd43546945f8448577d80
AndreaMcPherson <arm20@sage.southhills.edu>
2010-11-04 15:42:21 -0400

    hope this pushes :)

---
+++ plane.Designer.vb
@@ -0,0 +1,159 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnMap = New System.Windows.Forms.Button()
+        Me.lstMap = New System.Windows.Forms.ListBox()
+        Me.txtSeatNum = New System.Windows.Forms.TextBox()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.btnBookSeat = New System.Windows.Forms.Button()
+        Me.lblSeat = New System.Windows.Forms.Label()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.lstStandby = New System.Windows.Forms.ListBox()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnMap
+        '
+        Me.btnMap.Location = New System.Drawing.Point(433, 259)
+        Me.btnMap.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.btnMap.Name = "btnMap"
+        Me.btnMap.Size = New System.Drawing.Size(106, 34)
+        Me.btnMap.TabIndex = 0
+        Me.btnMap.Text = "Show Map"
+        Me.btnMap.UseVisualStyleBackColor = True
+        '
+        'lstMap
+        '
+        Me.lstMap.Font = New System.Drawing.Font("Courier New", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstMap.FormattingEnabled = True
+        Me.lstMap.ItemHeight = 16
+        Me.lstMap.Location = New System.Drawing.Point(33, 25)
+        Me.lstMap.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.lstMap.Name = "lstMap"
+        Me.lstMap.Size = New System.Drawing.Size(397, 196)
+        Me.lstMap.TabIndex = 1
+        '
+        'txtSeatNum
+        '
+        Me.txtSeatNum.Location = New System.Drawing.Point(190, 259)
+        Me.txtSeatNum.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.txtSeatNum.Name = "txtSeatNum"
+        Me.txtSeatNum.Size = New System.Drawing.Size(132, 22)
+        Me.txtSeatNum.TabIndex = 2
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(188, 349)
+        Me.txtName.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(132, 22)
+        Me.txtName.TabIndex = 3
+        '
+        'btnBookSeat
+        '
+        Me.btnBookSeat.Location = New System.Drawing.Point(433, 339)
+        Me.btnBookSeat.Name = "btnBookSeat"
+        Me.btnBookSeat.Size = New System.Drawing.Size(106, 32)
+        Me.btnBookSeat.TabIndex = 4
+        Me.btnBookSeat.Text = "Book Seat"
+        Me.btnBookSeat.UseVisualStyleBackColor = True
+        '
+        'lblSeat
+        '
+        Me.lblSeat.AutoSize = True
+        Me.lblSeat.Location = New System.Drawing.Point(36, 259)
+        Me.lblSeat.Name = "lblSeat"
+        Me.lblSeat.Size = New System.Drawing.Size(129, 17)
+        Me.lblSeat.TabIndex = 5
+        Me.lblSeat.Text = "Enter Seat Number"
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Location = New System.Drawing.Point(39, 349)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(83, 17)
+        Me.lblName.TabIndex = 6
+        Me.lblName.Text = "Enter Name"
+        '
+        'lstStandby
+        '
+        Me.lstStandby.FormattingEnabled = True
+        Me.lstStandby.ItemHeight = 16
+        Me.lstStandby.Location = New System.Drawing.Point(547, 29)
+        Me.lstStandby.Name = "lstStandby"
+        Me.lstStandby.Size = New System.Drawing.Size(120, 84)
+        Me.lstStandby.TabIndex = 7
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Location = New System.Drawing.Point(433, 419)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(106, 30)
+        Me.btnCancel.TabIndex = 8
+        Me.btnCancel.Text = "Cancel Seat"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(617, 348)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox1.TabIndex = 9
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(812, 476)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.btnCancel)
+        Me.Controls.Add(Me.lstStandby)
+        Me.Controls.Add(Me.lblName)
+        Me.Controls.Add(Me.lblSeat)
+        Me.Controls.Add(Me.btnBookSeat)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.txtSeatNum)
+        Me.Controls.Add(Me.lstMap)
+        Me.Controls.Add(Me.btnMap)
+        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Name = "frmMain"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnMap As System.Windows.Forms.Button
+    Friend WithEvents lstMap As System.Windows.Forms.ListBox
+    Friend WithEvents txtSeatNum As System.Windows.Forms.TextBox
+    Friend WithEvents txtName As System.Windows.Forms.TextBox
+    Friend WithEvents btnBookSeat As System.Windows.Forms.Button
+    Friend WithEvents lblSeat As System.Windows.Forms.Label
+    Friend WithEvents lblName As System.Windows.Forms.Label
+    Friend WithEvents lstStandby As System.Windows.Forms.ListBox
+    Friend WithEvents btnCancel As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+
+End Class
---
+++ plane.vb
@@ -0,0 +1,255 @@
+﻿Public Class frmMain
+    Dim strSeat(9, 3) As String
+    Dim intFull As Integer
+    Dim strStandby(5) As String
+    Dim n As Integer
+    Dim c As Integer
+
+    Private Sub btnMap_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnMap.Click
+
+        'build array
+        For i As Integer = 0 To 9
+            strSeat(n, 0) = c
+            n = n + 1
+            c = c + 1
+        Next
+        n = 0
+        For i As Integer = 0 To 9
+            strSeat(n, 1) = c
+            n = n + 1
+            c = c + 1
+        Next
+        n = 0
+        For i As Integer = 0 To 9
+            strSeat(n, 2) = c
+            n = n + 1
+            c = c + 1
+        Next
+        n = 0
+        For i As Integer = 0 To 9
+            strSeat(n, 3) = c
+            n = n + 1
+            c = c + 1
+        Next
+
+        'shows map
+        lstMap.Items.Clear()
+
+        For i As Integer = 0 To 9
+            lstMap.Items.Add(strSeat(i, 0) & "          " & strSeat(i, 1) & "                 " & strSeat(i, 2) & "           " & strSeat(i, 3))
+        Next
+
+    End Sub
+
+    Sub assignSeat()
+
+        If txtSeatNum.Text = 0 Then
+            strSeat(0, 0) = txtName.Text
+
+        ElseIf txtSeatNum.Text = 1 Then
+            strSeat(1, 0) = txtName.Text
+
+        ElseIf txtSeatNum.Text = 2 Then
+            strSeat(2, 0) = txtName.Text
+
+        ElseIf txtSeatNum.Text = 3 Then
+            strSeat(3, 0) = txtName.Text
+
+        ElseIf txtSeatNum.Text = 4 Then
+            strSeat(4, 0) = txtName.Text
+
+        ElseIf txtSeatNum.Text = 5 Then
+            strSeat(5, 0) = txtName.Text
+
+        ElseIf txtSeatNum.Text = 6 Then
+            strSeat(6, 0) = txtName.Text
+
+        ElseIf txtSeatNum.Text = 7 Then
+            strSeat(7, 0) = txtName.Text
+
+        ElseIf txtSeatNum.Text = 8 Then
+            strSeat(8, 0) = txtName.Text
+
+        ElseIf txtSeatNum.Text = 9 Then
+            strSeat(9, 0) = txtName.Text
+
+        End If
+
+    End Sub
+
+    Sub assignSeat1()
+        If txtSeatNum.Text = 10 Then
+            strSeat(0, 1) = txtName.Text
+            'intFull = intFull + 1
+        ElseIf txtSeatNum.Text = 11 Then
+            strSeat(1, 1) = txtName.Text
+            'intFull = intFull + 1
+        ElseIf txtSeatNum.Text = 12 Then
+            strSeat(2, 1) = txtName.Text
+            'intFull = intFull + 1
+        ElseIf txtSeatNum.Text = 13 Then
+            strSeat(3, 1) = txtName.Text
+            'intFull = intFull + 1
+        ElseIf txtSeatNum.Text = 14 Then
+            strSeat(4, 1) = txtName.Text
+            'intFull = intFull + 1
+        ElseIf txtSeatNum.Text = 15 Then
+            strSeat(5, 1) = txtName.Text
+            'intFull = intFull + 1
+        ElseIf txtSeatNum.Text = 16 Then
+            strSeat(6, 1) = txtName.Text
+            'intFull = intFull + 1
+        ElseIf txtSeatNum.Text = 17 Then
+            strSeat(7, 1) = txtName.Text
+            'intFull = intFull + 1
+        ElseIf txtSeatNum.Text = 18 Then
+            strSeat(8, 1) = txtName.Text
+            'intFull = intFull + 1
+        ElseIf txtSeatNum.Text = 19 Then
+            strSeat(9, 1) = txtName.Text
+            'intFull = intFull + 1
+        End If
+    End Sub
+
+    Sub assignSeat2()
+        If txtSeatNum.Text = 20 Then
+            strSeat(0, 2) = txtName.Text
+            ' intFull = intFull + 1
+        ElseIf txtSeatNum.Text = 21 Then
+            strSeat(1, 2) = txtName.Text
+            'intFull = intFull + 1
+        ElseIf txtSeatNum.Text = 22 Then
+            strSeat(2, 2) = txtName.Text
+            'intFull = intFull + 1
+        ElseIf txtSeatNum.Text = 23 Then
+            strSeat(3, 2) = txtName.Text
+            'intFull = intFull + 1
+        ElseIf txtSeatNum.Text = 24 Then
+            strSeat(4, 2) = txtName.Text
+            'intFull = intFull + 1
+        ElseIf txtSeatNum.Text = 25 Then
+            strSeat(5, 2) = txtName.Text
+            'intFull = intFull + 1
+        ElseIf txtSeatNum.Text = 26 Then
+            strSeat(6, 2) = txtName.Text
+            'intFull = intFull + 1
+        ElseIf txtSeatNum.Text = 27 Then
+            strSeat(7, 2) = txtName.Text
+            'intFull = intFull + 1
+        ElseIf txtSeatNum.Text = 28 Then
+            strSeat(8, 2) = txtName.Text
+            'intFull = intFull + 1
+        ElseIf txtSeatNum.Text = 29 Then
+            strSeat(9, 2) = txtName.Text
+            'intFull = intFull + 1
+        End If
+    End Sub
+
+    Sub assignSeat3()
+
+        If txtSeatNum.Text = 30 Then
+            strSeat(0, 3) = txtName.Text
+            'intFull = intFull + 1
+        ElseIf txtSeatNum.Text = 31 Then
+            strSeat(1, 3) = txtName.Text
+            'intFull = intFull + 1
+        ElseIf txtSeatNum.Text = 32 Then
+            strSeat(2, 3) = txtName.Text
+            'intFull = intFull + 1
+        ElseIf txtSeatNum.Text = 33 Then
+            strSeat(3, 3) = txtName.Text
+            'intFull = intFull + 1
+        ElseIf txtSeatNum.Text = 34 Then
+            strSeat(4, 3) = txtName.Text
+            'intFull = intFull + 1
+        ElseIf txtSeatNum.Text = 35 Then
+            strSeat(5, 3) = txtName.Text
+            'intFull = intFull + 1
+        ElseIf txtSeatNum.Text = 36 Then
+            strSeat(6, 3) = txtName.Text
+            'intFull = intFull + 1
+        ElseIf txtSeatNum.Text = 37 Then
+            strSeat(7, 3) = txtName.Text
+            'intFull = intFull + 1
+        ElseIf txtSeatNum.Text = 38 Then
+            strSeat(8, 3) = txtName.Text
+            'intFull = intFull + 1
+        ElseIf txtSeatNum.Text = 39 Then
+            strSeat(9, 3) = txtName.Text
+            'intFull = intFull + 1
+        End If
+    End Sub
+
+    Sub bookSeat()
+        'adds item to counter, selects where the seat is
+        intFull += 1
+        lstMap.Items.Clear()
+        Select Case txtSeatNum.Text
+            Case 0 To 9
+                assignSeat()
+        End Select
+        Select Case txtSeatNum.Text
+            Case 10 To 19
+                assignSeat1()
+        End Select
+        Select Case txtSeatNum.Text
+            Case 20 To 29
+                assignSeat2()
+        End Select
+        Select Case txtSeatNum.Text
+            Case 30 To 39
+                assignSeat3()
+        End Select
+
+        'updates map
+        For i As Integer = 0 To 9
+            lstMap.Items.Add(strSeat(i, 0) & "          " & strSeat(i, 1) & "                 " & strSeat(i, 2) & "           " & strSeat(i, 3))
+        Next
+    End Sub
+
+    Sub Standby()
+        'puts people on standby when the plane is full
+        If intFull = 3 Then
+            strStandby(0) = txtName.Text
+            lstStandby.Items.Add(strStandby(0))
+            intFull = intFull + 1
+        ElseIf intFull = 4 Then
+            strStandby(1) = txtName.Text
+            lstStandby.Items.Add(strStandby(1))
+            intFull += 1
+        ElseIf intFull = 5 Then
+            strStandby(2) = txtName.Text
+            lstStandby.Items.Add(strStandby(2))
+            intFull += 1
+        ElseIf intFull = 6 Then
+            strStandby(3) = txtName.Text
+            lstStandby.Items.Add(strStandby(3))
+            intFull += 1
+        End If
+
+    End Sub
+
+    Private Sub btnBookSeat_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnBookSeat.Click
+
+        If intFull >= 3 Then 'this means full
+            Standby()
+        Else
+            bookSeat()
+        End If
+
+    End Sub
+
+    Private Sub btnCancel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCancel.Click
+
+        If intFull >= 4 Then
+            txtName.Text = strStandby(0)
+            lstStandby.Items.Remove(strStandby(0))
+            bookSeat()
+            strStandby(0) = strStandby(1)
+        Else
+            txtName.Text = txtSeatNum.Text
+            bookSeat()
+        End If
+
+    End Sub
+End Class
